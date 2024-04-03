657daef8bea4f5c6d6679bf69f54d677f4672285
牛洪法 <hongfa.n@linkingcloud.cn>
2024-04-03 20:52:24 +0800

    fix

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,19 @@ export const genAlphaColor = getAlphaColor;
  // getAlphaColor;
 export { createThemeStore, createThemeList } from "./theme/index"
 
+export type { ThemeOptions, ThemeType } from "./theme/index"
+
+export type {
+    SizeMap,
+    StyleMap,
+    SeedMap,
+    SeedOption,
+    FontMap,
+    PresetColorKey,
+    PresetColorType,
+    ColorPalettes
+} from "./theme/interface";
+
 export type ColorToken = MapToken;
 
 
--- src2/index.ts
+++ src2/index.ts
@@ -5,6 +5,18 @@ export const genAlphaColor = getAlphaColor;
  // getAlphaColor;
 export { createThemeList } from "../src/theme/index2"
 
+export type { ThemeOptions, ThemeType } from "../src/theme/index"
+
+export type {
+    SizeMap,
+    StyleMap,
+    SeedMap,
+    SeedOption,
+    FontMap,
+    PresetColorKey,
+    PresetColorType,
+    ColorPalettes
+} from "../src/theme/interface";
 export type ColorToken = MapToken;
 
 
